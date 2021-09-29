f0a9ef611bcf0a8d55bd32633495fb1386a0c65f
khanhhoa <truongkhanhhoa623@gmail.com>
2021-09-29 23:44:57 +0700

    d

--- src/index.js
+++ src/index.js
@@ -1,48 +1,50 @@
-const express = require("express")
-const morgan = require("morgan")
-const handlebars = require("express-handlebars")
-const methodOverride = require("method-override") 
-const app = express()
-const route = require("./routes")
+const express = require("express");
+const morgan = require("morgan");
+const handlebars = require("express-handlebars");
+const methodOverride = require("method-override");
+const app = express();
+const route = require("./routes");
 const port = process.env.PORT || 3000;
-const db = require("./config/db")
-const sortMiddleware = require("./middlewares/sortMiddleware")
-const path = require('path')
+const db = require("./config/db");
+const sortMiddleware = require("./middlewares/sortMiddleware");
+const path = require("path");
 
-db.connect()
-app.use(express.static(path.join(__dirname, "public", "")))
+db.connect();
+app.use(express.static(path.join(__dirname, "public", "")));
 app.engine(
   "hbs",
   handlebars({
     extname: "hbs",
     helpers: {
       sum: (number1, number2) => number1 + number2,
-      sortable: (column, sort)=>{
-        const iconSort = column === sort.column? sort.type : 'default'
+      sortable: (column, sort) => {
+        const iconSort = column === sort.column ? sort.type : "default";
         const icons = {
-            default: "oi oi-elevator",
-            desc: "oi oi-sort-descending",
-            asc: "oi oi-sort-ascending"
-        }
+          default: "oi oi-elevator",
+          desc: "oi oi-sort-descending",
+          asc: "oi oi-sort-ascending",
+        };
         const types = {
-            default: "desc",
-            desc: "asc",
-            asc: "desc"
-        }
-        const icon = icons[iconSort]
-        const type = types[sort.type]
-        return `<a href="/me/stored/post/?_sort&column=${column}&type=${type}"><span class="${icon}"></span></a> `
-      }
-    }
-  }),
- 
+          default: "desc",
+          desc: "asc",
+          asc: "desc",
+        };
+        const icon = icons[iconSort];
+        const type = types[sort.type];
+        return `<a href="/me/stored/post/?_sort&column=${column}&type=${type}"><span class="${icon}"></span></a> `;
+      },
+    },
+  })
 );
-app.use(sortMiddleware)
-app.use(express.urlencoded())
-app.set("view engine", "hbs")
-app.set("views", path.join(__dirname, "resources", "views"))
+app.use(sortMiddleware);
+app.use(express.urlencoded());
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources", "views"));
 // app.use(morgan('combined'))
-app.use(methodOverride("_method"))
-route(app)
+app.use(methodOverride("_method"));
+route(app);
 
-app.listen(port, () => console.log(`app listeing port: ${port}`))
+app.listen(port, () => {
+  const port = server.address().port;
+  console.log(`app listeing port: ${port}`);
+});
